a4379314c1805cbaa3df1ed91f67bff6c7ce6c97
Manahill <manahhil213@gmail.com>
2025-09-01 00:28:26 -0700

    Update Web-App.py

--- notebook/Web-App.py
+++ notebook/Web-App.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
-import tensorflow as tf   
+import tflite_runtime.interpreter as tflite
+
 
 # ----------------- CONFIG -----------------
 st.set_page_config(page_title="Cats vs Dogs Classifier",
